5a8bc3a451c1258cc73f1fe092880ffecad43a26
hochan222 <hochan049@gmail.com>
2021-04-09 10:57:19 +0900

    [REFACTOR] movie api

--- movie_api/script.js
+++ movie_api/script.js
@@ -1,6 +1,8 @@
+// http://www.kobis.or.kr/kobisopenapi/homepg/apiservice/searchServiceInfo.do
 url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101"
 
 function status(response) {
+    /* Err Handling */
     console.log(response.status + " " + response.statusText);
     return Promise.resolve(response)
 }
@@ -11,15 +13,12 @@ function parseText(res) {
 
 function getBody(data) {
     var result = JSON.parse(data).boxOfficeResult;
-    var movielist;
 
-    console.log(result.boxofficeType)
-    // document.getElementById("boxofficeType").innerText = result.boxofficeType;
+    // console.log(result.boxofficeType)
     for (let i=0; i<result.dailyBoxOfficeList.length; i++)
     {
         document.getElementById("boxofficeType").innerText += result.dailyBoxOfficeList[i]["movieNm"] + '\n';
     }
-    // console.log(result.dailyBoxOfficeList[0]["movieNm"]);
 }
 
 fetch(url)
